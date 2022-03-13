2c2b42efb210a78d851db865269e86336a053348
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2022-03-13 17:54:10 +0100

    s

--- propdrillingVScont/react-context/README.md
+++
@@ -1,3 +0,0 @@
-# React Context Tutorial
-
-tutorial available on youtube: https://www.youtube.com/watch?v=hUhWtYXgg0I&ab_channel=CodingWithAdam
--- propdrillingVScont/react-context/src/components/footer/FooterBranding.js
+++
@@ -1,9 +0,0 @@
-const FooterBranding = () => {
-  return (
-    <p className="col-md-4 mb-0 text-muted">
-      © {new Date().getFullYear()} Coffee House
-    </p>
-  );
-};
-
-export default FooterBranding;
